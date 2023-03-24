2631e00e042e1d8bd623956ea72e88641674dbb6
EmmGlezz <contact@emilianogonzalez.com>
2023-03-24 21:54:49 +1100

    OpenAi prompts modified

--- server/controllers/destination.js
+++ server/controllers/destination.js
@@ -24,14 +24,14 @@ const getDestinationInfo = async (req, res) => {
         
         const travelParameters = `With this json object, plan my trip including names and links to external resources (like websites, and images or videos), and format it on json too
         make sure that the json object contains the next (IMPORTANT, ONLY RETURN A JSON OBJECT):
-        - Name of the destination, state, country, description, nearest airport, coordinates, best months to travel in (json key has to be named best_months_to_travel_in), local currency, timezone, and website.
+        - Name of the destination, state, country, description, nearest airport, coordinates, best months to travel in (json key has to be named best_months_to_travel_in, it has to be an array with string of the months), local currency, timezone, and website.
         - Current location, including name, nearest airport, country.
         - The dates.
         - The budget, including currency.
-        - At least 3 different options for accomodations, and each accomodation includes the name,  website, star rating and average night cost with currency.
-        - At least 3 different options to get from current location to destination, like plane tickets, ships, or any other type, and each option includes the name, type, website, and cost with currency.
-        - At least 2 types of transportation locally, and each transportation includes, the type, and a website.
-        - At least 5 different activities, and each activity includes the type, short description, average cost with currency, and a website.
+        - At least 3 different options for accomodations (IMPORTANT json key has to be named accomodations WITH JUST ONE M BETWEEN THE O'S), and each accomodation includes the name,  website, star rating and average night cost (json key has to be named average_night_cost) with currency.
+        - At least 3 different options to get from current location to destination (json property has to be named transportation_to_destination), like plane tickets, ships, or any other type, and each option includes the name, type, website, and cost with currency.
+        - At least 2 types of transportation locally (json property has to be named local_transportation), and each transportation includes, the type, and a website.
+        - At least 5 different activities (json property has to be named activities), and each activity includes the type, short description, average cost (json property has to be named amount) with currency, and a website.
         - Group style.
         - Travel style. 
         : ${JSON.stringify(userParameters)}`
