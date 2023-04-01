b1f4d143e8d3f52da037262409b3fbf86ee5ef1d
EmmGlezz <contact@emilianogonzalez.com>
2023-04-01 19:20:25 +1100

    API deployment test 0.5

--- server/controllers/destination.js
+++ server/controllers/destination.js
@@ -34,7 +34,7 @@ const getDestinationInfo = async (req, res) => {
         })
         const data = await JSON.parse(response.data.choices[0].message.content)
         console.log(data)
-        const images = await fetch(`https://api.unsplash.com/search/photos/?query=${data.destination.name.toLowerCase().split(' ').join('-')}&client_id=${process.env.UNSPLASH_KEY}&page=1&per_page=4`, {
+        const images = await fetch(`https://api.unsplash.com/search/photos/?query=paris&client_id=${process.env.UNSPLASH_KEY}&page=1&per_page=4`, {
             method: 'GET'
         })
 
@@ -52,3 +52,4 @@ const getDestinationInfo = async (req, res) => {
 
 
 module.exports = { getDestinationInfo }
+// ${data.destination.name.toLowerCase().split(' ').join('-')}
